132bc3a876170fa6736e02bb372758b3b3a98b3c
j_dim <dmitrijsuplev@gmail.com>
2025-06-02 18:34:23 +0300

    add_button_to_visual_interface2

--- src/view/ButtonPanel.java
+++ src/view/ButtonPanel.java
@@ -10,23 +10,33 @@ public class ButtonPanel extends JPanel {
     public ButtonPanel() {
         setBackground(Color.BLUE);
         JButton startButton = new JButton("Старт");
-        JButton finishButton = new JButton("Финиш");
+        JButton pauseButton = new JButton("Приостановить");
+
+        JButton finishButton = new JButton("Закончить");
 
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//добавить логику кнопки
-
+                System.out.println("Старт");
            }
+        });
+
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Пауза");
+            }
         });
 
         finishButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               //Добавить логику кнопки
+               System.exit(1);
             }
         });
+
         add(startButton);
+        add(pauseButton);
         add(finishButton);
     }
 
