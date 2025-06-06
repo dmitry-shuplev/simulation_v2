ce94656d6fda5b727045ae53ef9cc70e180b07aa
j_dim <dmitrijsuplev@gmail.com>
2025-06-06 07:50:44 +0300

    add_interface_movable

--- src/app_main/Settings.java
+++ src/app_main/Settings.java
@@ -6,8 +6,8 @@ public class Settings {
     public static final int VIEW_WIDTH = 1200;
     public static final int VIEW_HEIGHT = 600;
 
-    public static final int GAME_FIELD_X_MAX = 50;
-    public static final int GAME_FIELD_Y_MAX = 20;
+    public static final int GAME_FIELD_X_MAX = 40;
+    public static final int GAME_FIELD_Y_MAX = 15;
     public static final int GRASS_QUANTITY = 1;
     public static final int HERBIVORE_HEALTH = 1;
     public static final int PREDATOR_HEALTH = 1;
@@ -19,6 +19,14 @@ public class Settings {
             "Tree", 20,
             "Rock", 50
     );
+
+    public enum Direction {
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT
+    }
+
 
 }
    
